055beb670f9804e87eefd6a96a5526f9eda3eb89
Othniel Timothy De Mesa Luzon <otluzon@uia.no>
2025-02-06 18:22:49 +0100

    imported libraries bs4, requests, lxml; tested on NRK rss

--- main/main.py
+++ main/main.py
@@ -1,5 +1,21 @@
+import requests
+from bs4 import BeautifulSoup
+
+def request():
+    response = requests.get('https://www.nrk.no/rogaland/toppsaker.rss')
+    return response
+
 def main():
-    print("Hello, World!")
+    nrk = request()
+    print(nrk)
+    
+    soup = BeautifulSoup(nrk.content, 'xml')
+    # print(soup.prettify())
+
+    titles = soup.find_all('title')
+    for title in titles:
+        print(title.text)
+    # print("Hello, World!")
 
 if __name__ == "__main__":
     main()
