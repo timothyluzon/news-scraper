cfef126a831a8ce0ecafedfeb4059ae5a010c4d4
Othniel Timothy De Mesa Luzon <otluzon@uia.no>
2025-02-07 11:38:43 +0100

    added functions for extracting title and description from rss items

--- main/main.py
+++ main/main.py
@@ -1,21 +1,29 @@
 import requests
 from bs4 import BeautifulSoup
 
-def request():
-    response = requests.get('https://www.nrk.no/rogaland/toppsaker.rss')
+test_url = 'https://www.nrk.no/rogaland/toppsaker.rss'
+
+# request the rss feed from news site
+def request(url: str):
+    response = requests.get(url)
     return response
 
+# parse the rss response, return the titles and description
+def rss_parser(rss):
+    soup = BeautifulSoup(rss.content, 'xml')
+    items = soup.find_all('item')
+    return items
+
 def main():
-    nrk = request()
-    print(nrk)
+    response = request(test_url)
+    print(response)
     
-    soup = BeautifulSoup(nrk.content, 'xml')
-    # print(soup.prettify())
+    # soup = BeautifulSoup(response.content, 'xml')
 
-    titles = soup.find_all('title')
-    for title in titles:
-        print(title.text)
-    # print("Hello, World!")
+    contents = rss_parser(response)
+    for content in contents:
+        print(f'title: {content.title.text}')
+        print(f'description: {content.description.text}')
 
-if __name__ == "__main__":
+if __name__ == "__main__":  
     main()
