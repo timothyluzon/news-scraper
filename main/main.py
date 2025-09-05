abf9c17e998da696343270ea40befd51c508d499
Othniel Timothy Luzon <otluzon@uia.no>
2025-09-05 14:43:51 +0200

    added function to parse feed, can print the title of the feed, the title of each item in the feed and the description (if any)

--- main/main.py
+++ main/main.py
@@ -1,13 +1,28 @@
 import requests;
+from bs4 import BeautifulSoup;
 
-def get_feed():
-    res = requests.get('https://www.nrk.no/rogaland/toppsaker.rss')
+def get_feed(url: str):
+    res = requests.get(url)
     if (res.status_code == 200):
         print(f'{res.status_code} : OK!')
+        return(res.text)
+    else:
+        print(f'{res.status_code}')
+        print(f'Feed Not Obtained from {url}')
+
+def parse_feed(feed):
+    soup = BeautifulSoup(feed, 'xml')
+    print(soup.title)
+    for item in soup.find_all('item'):
+        print(item.title.string)
+        print(item.description.string)
+        print('\n')
 
 def main():
     print("Hello, World!")
-    get_feed()
+    url = 'https://www.nrk.no/rogaland/toppsaker.rss'
+    feed = get_feed(url)
+    parse_feed(feed)
 
 if __name__ == "__main__":
     main()
