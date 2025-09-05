50684e94f087b326c41462d3982fdcb5d15bf192
Othniel Timothy Luzon <otluzon@uia.no>
2025-09-05 14:08:33 +0200

    added get_feed() function to fetch rss feed, currently only one feed

--- main/main.py
+++ main/main.py
@@ -1,5 +1,13 @@
+import requests;
+
+def get_feed():
+    res = requests.get('https://www.nrk.no/rogaland/toppsaker.rss')
+    if (res.status_code == 200):
+        print(f'{res.status_code} : OK!')
+
 def main():
     print("Hello, World!")
+    get_feed()
 
 if __name__ == "__main__":
     main()
